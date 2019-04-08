c08765471ad3195c8a80ed35d107060b07beac04
ASEDOS999 <kuruzov2014@mail.ru>
2019-04-08 18:21:11 +0300

    Add class for action and announce the main functions
    Class for action is the 'class action'. It involves attribute that is verb, subject and object of action, etc., and functions for to get some attribute.
    
    Function get_actions will get list of action found in tree that was built on sentence. Other function is help function for 'get_actions' and they will be written after.

---
+++ action.py
@@ -0,0 +1,75 @@
+class action:
+	name_action = 'Action'
+	
+	def __init__(self, verb, object = [], subject = [], time = [], place = [], way = []):
+		self.verb = verb
+		self.object = object
+		self.subject = subject
+		self.time = time
+		self.place = place
+		self.way = way
+	
+	def get_verb(self):
+		return self.verb
+	
+	def get_object(self):
+		return self.object
+	
+	def get_subject(self):
+		return self.subject
+	
+	def get_time(self):
+		return self.time
+	
+	def get_place(self):
+		return self.place
+	
+	def get_way(self):
+		return self.way
+
+def is_verb(x):
+	return True
+
+def action_verb(x)
+	def is_not_modal():
+		return True
+	
+	def is_indicative():
+		return True
+	
+	return is_not_modal() and is_indicative()
+
+class tree:
+	def __init__(self, value):
+		self.value = value
+		self.kids = []
+
+	# The arguments of this function is a value of new vertices
+	# and type of relationship between parent and kid
+	def add_child(self, value, type is None):
+		self.kids.append((value, type))
+
+def process_type(vert, act):
+	# Some actions
+	return True
+
+def get_actions(sentence, root):
+	all_actions = []
+	
+	def research(parent):
+		list = []
+		for i in parent.children:
+			list.append(i)
+			if is_verb(i[0]) and action_verb(i[0]):
+				obj = parent[0]
+				information = research(i)
+				act = action(verb = i[0], object = obj)
+				for j in information:
+					process_type(j, act)
+				all_actions.append(act)
+			else:
+				research(i)
+		return list
+	
+	research(root)
+	return all_actions
