fc0bd9b6b3dde998f48b34b8bf5e986b8e424a89
ASEDOS999 <kuruzov2014@mail.ru>
2019-04-10 19:02:26 +0300

    Change structure. Fix little bag
    In this commit one changed order of functions and  classes and added new function for to construct tree.
    
    Also one fixed a little bag caused by not right using of tree's structure.

--- action.py
+++ action.py
@@ -1,12 +1,27 @@
+class tree:
+	def __init__(self, value):
+		self.value = value
+		self.kids = []
+
+	# The arguments of this function is a value of new vertices
+	# and type of relationship between parent and kid
+	def add_child(self, value, type = None):
+		self.kids.append((tree(value), type))
+
+def construct_tree(tree, sentence):
+	root = tree(tree[0], sentence[0])
+	return root
+
 class action:
 	name_action = 'Action'
 	
-	def __init__(self, verb, object = [], subject = [], time = [], place = [], way = []):
+	def __init__(self, verb, subject = [], object = [], time = [], place = [], purpose = [], way = []):
 		self.verb = verb
 		self.object = object
 		self.subject = subject
 		self.time = time
 		self.place = place
+		self.purpose = purpose
 		self.way = way
 	
 	def get_verb(self):
@@ -24,6 +39,9 @@ class action:
 	def get_place(self):
 		return self.place
 	
+	def get_purpose(self):
+		return self.purpose
+	
 	def get_way(self):
 		return self.way
 
@@ -39,24 +57,20 @@ def action_verb(x):
 	
 	return is_not_modal() and is_indicative()
 
-class tree:
-	def __init__(self, value):
-		self.value = value
-		self.kids = []
-
-	# The arguments of this function is a value of new vertices
-	# and type of relationship between parent and kid
-	def add_child(self, value, type = None):
-		self.kids.append((value, type))
-
 def process_type(vert, act):
-	object_type = []
-	subject_type = []
-	time_type = []
-	place_type = []
-	way_type = []
-	array = [object_type, subject_type, time_type, place_type, way_type]
-	answer = [False, False, False, False, False]
+	object_type = ['dobj', 'iobj', 'xcomp']
+	subject_type = ['agent', 'nsubj', 'xsubj']
+	time_type = ['tmod']
+	place_type = ['advcl']
+	purpose_type = ['advcl']
+	way_type = ['acomp']
+	array = [object_type, 
+		subject_type, 
+		time_type, 
+		place_type, 
+		purpose_type,
+		way_type]
+	answer = [False] * len(array)
 	for i in range(len(array)):
 		answer[i] = True if vert[1] in array[i] else False
 	def _(i):
@@ -68,24 +82,29 @@ def process_type(vert, act):
 	act.subject.append(vert[0]) if answer[1] and _(1) else None
 	act.time.append(vert[0]) if answer[2] and _(2) else None
 	act.place.append(vert[0]) if answer[3] and _(3) else None
-	act.way.append(vert[0]) if answer[4] and _(4) else None
+	act.purpose.append(vert[0]) if answer[4] and _(4) else None
+	act.way.append(vert[0]) if answer[5] and _(5) else None
+
+def get_inform_parent(parent, act):
+	return 0
+
 
 def get_actions(sentence, root):
 	all_actions = []
 	
 	def research(parent):
 		list = []
-		for i in parent.children:
+		for i in parent.kids:
 			list.append(i)
 			if is_verb(i[0]) and action_verb(i[0]):
-				obj = parent[0]
-				information = research(i)
-				act = action(verb = i[0], object = obj)
+				act = action(verb = i[0].value)
+				get_inform_parent(parent, act)
+				information = research(i[0])
 				for j in information:
 					process_type(j, act)
 				all_actions.append(act)
 			else:
-				research(i)
+				research(i[0])
 		return list
 	
 	research(root)
