5eb67e496e4ab9af80cc99ce27ca597bfe986d94
ASEDOS999 <kuruzov2014@mail.ru>
2019-04-13 16:31:50 +0300

    Add function for extracting data and features
    Add function for extracting data from tree and for extracting features from data. Change function for saving dataframe.

--- Process_type/extract.py
+++ Process_type/extract.py
@@ -1,9 +1,13 @@
 import sys
 sys.path.append(',,')
 from action import action_verb
+import numpy as np
+import pandas as pd
 
 def get_objects(root_list):
-	list_ = []
+	# Function for to extract list of kids and parent of verb from syntactic tree
+	# Input is a list of tree roots
+	# Output is a lists of kids and parents	list_ = []
 	parent_verb = []
 	def research(parent, mytype = None):
 		ret = False
@@ -19,6 +23,7 @@ def get_objects(root_list):
 	return list_, parent_verb
 
 def get_feat(i):
+	# Extract required information from all information about one word
 	feat = []
 	feat.append(i[1])
 	x = i[0]
@@ -28,8 +33,8 @@ def get_feat(i):
 		feat.append(x.value.postag)
 	return feat
 
-import pandas as pd
-def extract(root_list):
+def extract_data(root_list):
+	# Extract data from list of tree for each tree each tree, return dataframe
 	list_, parent_verb = get_objects(root_list)
 	list_feat_ = []
 	for i in list_:
@@ -38,6 +43,31 @@ def extract(root_list):
 	df = pd.DataFrame(data = 
 			{'c%d'%(i): [list_feat_[j][i] for j in range(len(list_feat_))] 
 				for i in range(len(list_feat_[0]))})
-	df.to_csv('features.csv', sep='\t', encoding='utf-8')
 	return df
 
+def extract_feat_from_dataframe(dataframe):
+	list_ = [[] for i in range(len(df.columns.values.tolist()))]
+	for j in range(len(df.columns.values.tolist())):
+		for i in df[df.columns.values[j]]:
+			if not i in list_[j]:
+				list_[j].append(i)
+
+	N = len(list_[0]) + len(list_[1])
+
+	array = None
+	for i in range(len(df)):
+		x = [0] * N
+		sum_ = 0
+		for j in range(len(df.columns.values.tolist())):
+			x[list_[j].index(df[df.columns.values[j]][i]) + sum_] = 1
+			sum_ += len(list_[j])
+		if array is None:
+			array = np.array(x)
+		else:
+			array = np.vstack((array, x))
+	return array
+
+def create_dataframe(root_list, name = 'features.csv'):
+	# Create and save dataframe as *.csv
+	df = extract_data(root_list)
+	df.to_csv(name, sep = '\t', encoding = 'utf')
