cb85c755d0c60e916d3c833417bdfa987c0f96ec
ASEDOS999 <kuruzov2014@mail.ru>
2019-04-15 15:49:19 +0300

    Add opportunity to get a phrase depending on verb

--- action.py
+++ action.py
@@ -49,16 +49,48 @@ def construct_tree(text):
 				root_list.append(list_[j])
 	return root_list
 
+def construct_sentence(list_word):
+	sentence = ''
+	for i in list_word:
+		sentence = sentence + i + ' '
+	return sentence
+
 class action:
 	def __init__(self, verb, sentence = None, name = None):
 		# self.keys = ['VERB', 'SUBJECT', 'OBJECT', 'TIME', 'PLACE', 'PURPOSE', 'WAY']
 		self.keys = ['VERB', 'SUBJECT', 'OBJECT', 'OTHER']
 		self.inform = dict()
-		for i in range(len(self.keys)):
-			self.inform[self.keys[i]] = []
+		for i in self.keys:
+			self.inform[i] = []
 		self.inform['VERB'] = verb
 		self.name_action = name
-		self.action_sentence = sentence
+		self.sentence = sentence
+	
+	def phrase(self, list_index):
+		list_word = [self.sentence[i] for i in list_index]
+		return construct_sentence(list_word)
+	
+	def extract_data_from_dict(self, list_):
+		ret_list = []
+		for j in range(len(list_)):
+			if list_[j]:
+				dict_ = dict()
+				for i in self.keys:
+					if i != 'VERB':
+						dict_[i] = []
+						for k in self.inform[i]:
+							if j == 1:
+								dict_[i].append(self.phrase(k[j]))
+							else:
+								dict_[i].append(k[j])
+					else:
+						dict_[i] = self.inform[i]
+				ret_list.append(dict_)
+		return ret_list
+	
+	def get_inform(self, main_word = False, full_inform = False, depend_dict = False):
+		list_ = [main_word, full_inform, depend_dict]
+		return self.extract_data_from_dict(list_)
 
 def action_verb(x):
 	def is_verb():
@@ -77,12 +109,27 @@ def action_verb(x):
 	
 	return is_verb() and is_indicative() and not is_modal()
 
-def not_inform(vert):
-	list_postag = ['PUNCT', 'CCONJ']
-	return vert[0].value.postag in list_postag
+def ignore_word(vert):
+	def not_inform():
+		list_postag = ['PUNCT', 'CCONJ']
+		return vert[0].value.postag in list_postag
+	return not_inform() or action_verb(vert[0])
+
+def extract_inform(vert, sentence):
+	main_word, depend = vert[0].value, vert[1]
+	list_index = [main_word.index]
+	def search(root):
+		for i in root.kids:
+			j = i[0]
+			if i[1] != 'punct':
+				list_index.append(j.value.index)
+			search(j)
+	search(vert[0])
+	list_index.sort()
+	return (main_word, list_index, depend)
 
 def process_type(vert, act):
-	if not_inform(vert):
+	if ignore_word(vert):
 		return 0
 	subject_type = ['agent', 'nsubj', 'xsubj']
 	object_type = ['dobj', 'iobj', 'obj']
@@ -93,11 +140,10 @@ def process_type(vert, act):
 		answer[i] = i + 1 if vert[1] in array[i] else 0
 	if np.array(answer).sum() == 0:
 		answer[-1] = len(array) + 1
-	act.inform[act.keys[np.array(answer).sum()]].append(vert[0].value)
+	act.inform[act.keys[np.array(answer).sum()]].append(extract_inform(vert, act.sentence))
 
 def get_inform_parent(parent, act):
 	return 0
-
 
 def get_actions(root):
 	all_actions = []
