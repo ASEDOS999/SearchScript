043b3bd578a7c1c9705886ae4452ff6e750ec9d4
ASEDOS999 <kuruzov2014@mail.ru>
2019-09-15 13:43:49 +0300

    Add table of data

--- SemanticAnalysis/get_data.py
+++ SemanticAnalysis/get_data.py
@@ -8,6 +8,7 @@ import sem_analysis as sa
 import gensim
 import numpy as np
 import os
+import pickle
 def find_cite(text):
 	_ = text.split('\n')
 	segm = [i.split(' ') for i in _]
@@ -41,14 +42,12 @@ def clear(sentences):
 
 def splitting_text(text):
 	def mysplit(paragraph):
-		print(paragraph)
 		cur = [paragraph]
 		mylist = ['?','.','!']
 		for sym in mylist:
 			cur = [i.split(sym) for i in cur]
 			_ = list()
 			for i in cur:
-				print(cur)
 				for ind,j in enumerate(i):
 					if ind < len(i) - 1 and len(j) > 0:
 						j += sym
@@ -62,10 +61,20 @@ def splitting_text(text):
 	for i in paragraphes:
 		sentences += mysplit(i)
 	sentences = [i for ind,i in enumerate(sentences) if len(i) > 0]
-	print(sentences)
 	return clear(sentences)
 
+import time
 def get_table(list_files):
+	l = len(list_files)
+	if 'table.pickle' in os.listdir():
+		with open('table.pickle', 'rb') as f:
+			table = pickle.load(f)
+			f.close()
+	else:
+		table = dict()
+
+	list_files = [i for i in list_files if not i in table]
+	keys = [i.split('/')[-1] for i in list_files if not i.split('/')[-1] in table]
 	texts = dict()
 	for i in list_files:
 		f = open(i, 'r')
@@ -73,8 +82,10 @@ def get_table(list_files):
 		f.close()
 	texts = {key:find_cite(texts[key])[0] for key in texts}
 	texts = {key:splitting_text(texts[key]) for key in texts}
-	table = dict()
-	for key in texts:
+
+	for key in keys:
+		print(key)
+		s = time.time()
 		text = texts[key]
 		res = list()
 		for sent in text:
@@ -89,9 +100,25 @@ def get_table(list_files):
 			is_instr = 0
 			if len(instr_sentence) > 0 and sent[-1]!= '?':
 				is_instr = 1
-			sent_tag_ud = sa.tag_ud(sent)
+			try:
+				sent_tag_ud = sa.tag_ud(sent)
+			except Exception:
+				sent_tag_ud = list()
 			res.append((sent, sent_tag_ud, is_instr))
 		table[key] = res
+		print('Time',(time.time()-s)/60, 'min')
+		print('Processed: %d/%d'%(len(table.keys()), l))
+		with open('table.pickle', 'wb') as f:
+			pickle.dump(res, f)
+			f.close()
 	return table
+
+
 path = '../Texts/'
-get_table([path+i for i in os.listdir(path) if '.txt' in i])
+list_files = [path+i for i in os.listdir(path) if '.txt' in i]
+list_files.sort()
+list_files = [i for i in list_files if '2_' in i] + [i for i in list_files if not '2_' in i]
+res = get_table(list_files)
+with open('table.pickle', 'wb') as f:
+	pickle.dump(res, f)
+	f.close()
