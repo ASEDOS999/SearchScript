ee93e53b4589dee4212babd430ffa6467918ed69
ASEDOS999 <kuruzov2014@mail.ru>
2019-11-14 16:36:48 +0300

    Restore condition for non-determined imperative verbs

--- ScriptExtract/Preprocessing/TextProcessing.py
+++ ScriptExtract/Preprocessing/TextProcessing.py
@@ -295,11 +295,10 @@ def cond_instr(act):
 			if (morph.__contains__('Person') and morph['Person'] == '3' and 
 			morph.__contains__('Number') and morph['Number'] == 'Sing'):
 				return lemma != 'быть' and there_is_inf(act)
-			#if (morph.__contains__('Person') and morph['Person'] == '2' and
-			#morph.__contains__('Aspect') and morph['Aspect'] == 'Perf'
-			#and morph.__contains__('Tense') and morph['Tense'] == 'Imp'):
-			#	print('hi3', act.inform['VERB'][0].lemma)
-			#	return True
+			if (morph.__contains__('Person') and morph['Person'] == '2' and
+			morph.__contains__('Aspect') and morph['Aspect'] == 'Perf'
+			and morph.__contains__('Tense') and morph['Tense'] == 'Imp'):
+				return True
 	if (len(subj) == 1 and 
 		morph.__contains__('Person') and morph['Person'] == '2'):
 		if morph.__contains__('Aspect') and morph['Aspect'] == 'Imp':
