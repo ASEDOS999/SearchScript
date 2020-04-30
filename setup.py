74c7ee72609118c5f734a3457b823f4534feb4de
ASEDOS999 <kuruzov2014@mail.ru>
2020-04-30 17:12:00 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 16:57:08 2020
+
+@author: elias
+"""
+
+from setuptools import setup, find_packages
+ 
+setup(name='ScriptExtract',
+      version='0.0',
+      url='https://github.com/ASEDOS999/SearchScript',
+      license='MIT',
+      author='Gigi Sayfan',
+      author_email='kuruzov.ia@phystech.edu',
+      description='Script Extractions',
+      packages=find_packages(exclude=['ScriptExtract']),
+      long_description=open('README.md').read(),
+      zip_safe=False)
